2c3dff292e2073db74268fa174165ba06a06d9c7
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-04-03 21:33:37 +0430

    Add firebase for signup user

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
-import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { db } from '../firebase.config'
 const SignUp = () => {
     const [showPassword, setShowPassword] = useState(false)
     const [formData, setFormData] = useState({
@@ -20,6 +22,26 @@ const SignUp = () => {
         }))
     }
 
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const auth = getAuth()
+
+            const userCredentional = await createUserWithEmailAndPassword(auth, email, password)
+
+            const user = userCredentional.user
+
+            updateProfile(auth.currentUser, {
+                displayName: name
+            })
+
+            navigate('/ ')
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <>
             <div className="pageContainer">
@@ -28,8 +50,8 @@ const SignUp = () => {
                 </header>
 
                 <main>
-                    <form>
-                    <input
+                    <form onSubmit={onSubmit}>
+                        <input
                             type="text"
                             id="name"
                             className='nameInput'
