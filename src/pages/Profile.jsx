a4d23d42748366e105599bffd02fa2469fae0112
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-04-04 21:04:36 +0430

    Add userName in profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,12 @@
+import { getAuth } from 'firebase/auth'
+import { useEffect, useState } from 'react';
 const Profile = () => {
-    return ( <h1>Profile</h1> );
+    const [user, setUser] = useState({})
+    const auth = getAuth()
+    useEffect(() => {
+        setUser(auth.currentUser)
+    })
+    return user ? <h1>{user.displayName}</h1> : 'Not Logged In'
 }
- 
+
 export default Profile;
